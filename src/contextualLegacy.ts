d95d61e429d3e797e158851bb010de00fe4bd659
kobi <kobi.b@inner-active.com>
2018-11-17 21:53:36 +0200

    refactor contextual

--- src/contextualLegacy.ts
+++ src/contextualLegacy.ts
@@ -2,7 +2,7 @@ import * as propTypes from "prop-types"
 import * as React from "react"
 
 export function contextualLegacy(name: string) {
-  return function <P>(WrappedComponent: React.ComponentClass<P>) {
+  return function <P, S>(WrappedComponent: React.ComponentClass<P, S>) {
     WrappedComponent.contextTypes = WrappedComponent.contextTypes || {}
     WrappedComponent.contextTypes[name] = propTypes.any
     Object.defineProperty(WrappedComponent.prototype, "ctx", {
