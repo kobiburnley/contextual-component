d679fb9c9f6a8e8123d49d5fe959700f17e5cd31
kobi <kobi.b@inner-active.com>
2019-03-07 18:35:03 +0200

    feat contextual local

--- test/contextual.test.tsx
+++ test/contextual.test.tsx
@@ -7,7 +7,6 @@ import {createConsumer} from "../src/createConsumer"
 import {ButtonContext, Button} from "./button"
 import {createLinkComponent, LinkContext} from "./link"
 
-export const MyButton = contextual(null! as ButtonContext, true)(Button)
 
 const service = {
   navTo: (path: string) => {
@@ -19,44 +18,11 @@ const service = {
 }
 
 describe("contextual", function () {
-  it("should pass context", () => {
-
-    const WrappedLink = createLinkComponent()
-    const Link = contextual<LinkContext>({
-      navTo: () => {
-      }
-    }, true)(WrappedLink)
-
-    class App extends React.PureComponent {
-      render() {
-        return <CombineProviders providers={[Link.Provider, MyButton.Provider] as Providers<typeof service>}
-                                 value={service}>
-          <Link path="/where-to">
-            <MyButton>
-              One
-            </MyButton>
-          </Link>
-          <MyButton>
-            Two
-          </MyButton>
-        </CombineProviders>
-      }
-    }
-
-    const renderer = create(<App/>)
-    const linkInstance = renderer.root.findByType(WrappedLink)
-    const buttonInstances = renderer.root.findAll(node => node.instance instanceof Button)
-    expect(buttonInstances.length).to.eq(2)
-    expect(linkInstance.instance.ctx).to.eq(service)
-    expect(buttonInstances.every(e => e.instance.ctx === service)).to.eq(true)
-  })
-
   it("consumer set from outside", () => {
     const WrappedLink = createLinkComponent()
-    const Link = contextual<LinkContext>({
-      navTo: () => {
-      }
-    })(WrappedLink)
+    const Link = contextual<LinkContext>()(WrappedLink)
+
+    const MyButton = contextual<ButtonContext>()(Button)
 
     const {Consumer, Provider} = React.createContext(service)
     Link.Consumer = Consumer
@@ -87,10 +53,8 @@ describe("contextual", function () {
 
   it("should throw in render if consumer not set", () => {
     const WrappedLink = createLinkComponent()
-    const Link = contextual<LinkContext>({
-      navTo: () => {
-      }
-    }, false)(WrappedLink)
+    const Link = contextual<LinkContext>()(WrappedLink)
+    const MyButton = contextual<ButtonContext>()(Button)
 
     const {Consumer, Provider} = React.createContext(service)
 
@@ -114,6 +78,8 @@ describe("contextual", function () {
 
   it("create consumer", () => {
     const {Consumer, Provider} = React.createContext(service)
+    const MyButton = contextual<ButtonContext>()(Button)
+
     MyButton.Consumer = createConsumer(Consumer, value => ({theme: value.theme}))
 
     class App extends React.PureComponent {
